486f605c6666c739a09c2170009cd1234d6fc857
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-04-19 16:23:07 +0530

    Added kafka streaming spark support

--- src/main/java/org/example/EcommerceTransform.java
+++ src/main/java/org/example/EcommerceTransform.java
@@ -1,22 +1,35 @@
 package org.example;
 
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.spark.SparkConf;
+import org.apache.spark.internal.config.R;
 import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.regression.LinearRegressionModel;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.StreamingQuery;
+import org.apache.spark.sql.streaming.StreamingQueryException;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
 import org.example.repository.ArangoRepository;
+import org.example.service.KafkaService;
 import org.example.service.SpendingService;
 
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.TimeoutException;
 
 import static org.apache.spark.sql.functions.*;
 
@@ -50,7 +63,12 @@ public class EcommerceTransform {
         Dataset<Row> newData = spark.createDataFrame(rowList, schema);
 
         SpendingService.getYearlySpendingBySubscriptionLength(formattedData, newData).show();
-
         spark.stop();
+
+        try {
+            KafkaService.startKafkaStreaming();
+        } catch (TimeoutException | StreamingQueryException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/org/example/service/KafkaService.java
@@ -0,0 +1,47 @@
+package org.example.service;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+
+import java.util.concurrent.TimeoutException;
+
+public class KafkaService {
+    public static void startKafkaStreaming() throws TimeoutException, StreamingQueryException {
+        SparkSession spark = SparkSession.builder().master("local").appName("EmailFilter").getOrCreate();
+
+        spark.sparkContext().setLogLevel("ERROR");
+
+        Dataset<Row> df = spark.readStream().format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", "test").option("startingOffsets", "latest").load();
+
+        df.writeStream().outputMode("append").format("console").start().awaitTermination();
+
+//        SparkConf sparkConf = new SparkConf();
+//        sparkConf.setAppName("TestSparkStreaming");
+//        sparkConf.setMaster("local");
+////        sparkConf.set("spark.cassandra.connection.host", "127.0.0.1");
+//
+//        JavaStreamingContext streamingContext = new JavaStreamingContext(sparkConf, Durations.seconds(1));
+//
+//        Map<String, Object> kafkaParams = new HashMap<>();
+//        kafkaParams.put("bootstrap.servers", "localhost:9092");
+//        kafkaParams.put("key.deserializer", StringDeserializer.class);
+//        kafkaParams.put("value.deserializer", StringDeserializer.class);
+//        kafkaParams.put("group.id", "use_a_separate_group_id_for_each_stream");
+//        kafkaParams.put("auto.offset.reset", "latest");
+//        kafkaParams.put("enable.auto.commit", false);
+//        Collection<String> topics = Arrays.asList("test");
+//
+//        JavaInputDStream<ConsumerRecord<String, String>> messages = KafkaUtils.createDirectStream(streamingContext, LocationStrategies.PreferConsistent(), ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams));
+//
+//
+//        streamingContext.start();
+//
+//        try {
+//            streamingContext.awaitTermination();
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+}
