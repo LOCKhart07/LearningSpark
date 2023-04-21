9b62040883c20d2a397e7d7801e3a862c6e2ee05
Jenslee Dsouza <dsouzajenslee@gmail.com>
2023-04-21 19:40:12 +0530

    Removed unnecesary imports

--- src/main/java/org/example/EcommerceTransform.java
+++ src/main/java/org/example/EcommerceTransform.java
@@ -1,35 +1,12 @@
 package org.example;
 
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.spark.SparkConf;
-import org.apache.spark.internal.config.R;
-import org.apache.spark.ml.feature.VectorAssembler;
-import org.apache.spark.ml.regression.LinearRegressionModel;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.streaming.StreamingQuery;
-import org.apache.spark.sql.streaming.StreamingQueryException;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.Metadata;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
-import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaInputDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
-import org.apache.spark.streaming.kafka010.ConsumerStrategies;
-import org.apache.spark.streaming.kafka010.KafkaUtils;
-import org.apache.spark.streaming.kafka010.LocationStrategies;
 import org.example.repository.ArangoRepository;
 import org.example.service.KafkaService;
 import org.example.service.SpendingService;
-
-
-import java.util.*;
-import java.util.concurrent.TimeoutException;
 
 import static org.apache.spark.sql.functions.*;
 
